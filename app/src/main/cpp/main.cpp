bc5d05968febdd27cd6dcdec88dd28c0988d7f7e
laofu <lverniu777@163.com>
2020-04-21 23:55:39 +0800

    SDL渲染纹理矩形

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -1,79 +1,63 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
 
 extern "C" {
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
 }
 
-
-/**
- * XPM格式的图片，XPM以ASCII码的形式保存图片信息
- */
-static char *icon_xpm[] = {
-        "32 23 3 1",
-        "     c #FFFFFF",
-        ".    c #000000",
-        "+    c #FFFF00",
-        "                                ",
-        "            ........            ",
-        "          ..++++++++..          ",
-        "         .++++++++++++.         ",
-        "        .++++++++++++++.        ",
-        "       .++++++++++++++++.       ",
-        "      .++++++++++++++++++.      ",
-        "      .+++....++++....+++.      ",
-        "     .++++.. .++++.. .++++.     ",
-        "     .++++....++++....++++.     ",
-        "     .++++++++++++++++++++.     ",
-        "     .++++++++++++++++++++.     ",
-        "     .+++++++++..+++++++++.     ",
-        "     .+++++++++..+++++++++.     ",
-        "     .++++++++++++++++++++.     ",
-        "      .++++++++++++++++++.      ",
-        "      .++...++++++++...++.      ",
-        "       .++............++.       ",
-        "        .++..........++.        ",
-        "         .+++......+++.         ",
-        "          ..++++++++..          ",
-        "            ........            ",
-        "                                "};
-
 int main(int argc, char *argv[]) {
-    SDL_Window *window;
-    SDL_Renderer *renderer;
-    SDL_Surface *surface;
-    SDL_Texture *texture;
-    int done;
-    SDL_Event event;
-
-    if (SDL_CreateWindowAndRenderer(0, 0, 0, &window, &renderer) < 0) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-                     "SDL_CreateWindowAndRenderer() failed: %s", SDL_GetError());
-        return (2);
+    const char *TAG = "LFPlayer";
+    const int width = 1080;
+    const int height = 1920;
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        __android_log_print(ANDROID_LOG_ERROR, TAG, "sdl init error %s", SDL_GetError());
+        return -1;
     }
-
-    surface = IMG_ReadXPMFromArray(icon_xpm);
-    texture = SDL_CreateTextureFromSurface(renderer, surface);
-    if (!texture) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-                     "Couldn't load texture: %s", SDL_GetError());
-        return (2);
+    SDL_Window *sdlWindow = SDL_CreateWindow("HelloWorld", 0, 0, width, height, SDL_WINDOW_SHOWN);
+    if (sdlWindow == NULL) {
+        __android_log_print(ANDROID_LOG_ERROR, TAG, "create window error %s", SDL_GetError());
+        return -1;
     }
-    SDL_SetWindowSize(window, 800, 480);
-
-    done = 0;
-    while (!done) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT)
-                done = 1;
+    SDL_Renderer *sdlRenderer = SDL_CreateRenderer(sdlWindow, -1, 0);
+    if (sdlRenderer == NULL) {
+        __android_log_print(ANDROID_LOG_ERROR, TAG, "create renderer error %s", SDL_GetError());
+        return -1;
+    }
+    SDL_Event sdlEvent;
+    SDL_Texture *sdlTexture = SDL_CreateTexture(sdlRenderer, SDL_PIXELFORMAT_ABGR8888,
+                                                SDL_TEXTUREACCESS_TARGET, width, height);
+    if (sdlTexture == NULL) {
+        __android_log_print(ANDROID_LOG_ERROR, TAG, "create texture error %s", SDL_GetError());
+        return -1;
+    }
+    SDL_Rect sdlRect;
+    sdlRect.w = 200;
+    sdlRect.h = 200;
+    int exit = 0;
+    while (!exit) {
+        if (SDL_PollEvent(&sdlEvent)) {
+            if (sdlEvent.type == SDL_QUIT) {
+                exit = 1;
+            }
+            __android_log_print(ANDROID_LOG_ERROR, TAG, "sdl event %d", sdlEvent.type);
         }
-        SDL_RenderCopy(renderer, texture, NULL, NULL);
-        SDL_RenderPresent(renderer);
-        SDL_Delay(100);
+        SDL_SetRenderTarget(sdlRenderer, sdlTexture);
+        SDL_SetRenderDrawColor(sdlRenderer, 255, 255, 255, 255);
+        SDL_RenderClear(sdlRenderer);
+        sdlRect.x = rand() % (width - sdlRect.w);
+        sdlRect.y = rand() % (height - sdlRect.h);
+        SDL_SetRenderDrawColor(sdlRenderer, 255, 0, 0, 255);
+        SDL_RenderDrawRect(sdlRenderer, &sdlRect);
+        SDL_RenderFillRect(sdlRenderer, &sdlRect);
+        SDL_SetRenderTarget(sdlRenderer, NULL);
+        SDL_RenderCopy(sdlRenderer, sdlTexture, NULL, NULL);
+        SDL_RenderPresent(sdlRenderer);
     }
-    SDL_DestroyTexture(texture);
 
+    SDL_DestroyTexture(sdlTexture);
+    SDL_DestroyRenderer(sdlRenderer);
+    SDL_DestroyWindow(sdlWindow);
     SDL_Quit();
-    return (0);
+    return 0;
 }
