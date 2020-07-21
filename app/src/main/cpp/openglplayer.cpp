b6b501262379575be8f188c13890448b6d7ab064
laofu <lverniu777@163.com>
2020-07-22 00:16:22 +0800

    在Native层进行OpenGL渲染

--- app/src/main/cpp/openglplayer.cpp
+++ app/src/main/cpp/openglplayer.cpp
@@ -11,6 +11,92 @@
 #include <jni.h>
 #include <android/native_window.h>
 #include <android/native_window_jni.h>
+#include <cstdlib>
+
+const char *gVertexShader =
+        "attribute vec4 vPosition;\n"
+        "void main() {\n"
+        "  gl_Position = vPosition;\n"
+        "}\n";
+
+const char *gFragmentShader =
+        "precision mediump float;\n"
+        "void main() {\n"
+        "  gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);\n"
+        "}\n";
+
+EGLContext eglContext;
+EGLSurface eglSurface;
+
+const GLfloat gTriangleVertices[] = {
+        0.0f, 0.5f,
+        -0.5f, -0.5f,
+        0.5f, -0.5f
+};
+GLuint gProgram;
+GLint gvPositionHandle;
+
+GLuint loadShader(GLenum shaderType, const char *pSource) {
+    GLuint shader = glCreateShader(shaderType);
+    if (shader) {
+        glShaderSource(shader, 1, &pSource, NULL);
+        glCompileShader(shader);
+        GLint compiled = 0;
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+        if (!compiled) {
+            GLint infoLen = 0;
+            glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
+            if (infoLen) {
+                char *buf = (char *) malloc(infoLen);
+                if (buf) {
+                    glGetShaderInfoLog(shader, infoLen, NULL, buf);
+                    LOG("Could not compile shader %d:\n%s\n",
+                        shaderType, buf);
+                    free(buf);
+                }
+                glDeleteShader(shader);
+                shader = 0;
+            }
+        }
+    }
+    return shader;
+}
+
+GLuint createProgram(const char *pVertexSource, const char *pFragmentSource) {
+    GLuint vertexShader = loadShader(GL_VERTEX_SHADER, pVertexSource);
+    if (!vertexShader) {
+        return 0;
+    }
+
+    GLuint pixelShader = loadShader(GL_FRAGMENT_SHADER, pFragmentSource);
+    if (!pixelShader) {
+        return 0;
+    }
+
+    GLuint program = glCreateProgram();
+    if (program) {
+        glAttachShader(program, vertexShader);
+        glAttachShader(program, pixelShader);
+        glLinkProgram(program);
+        GLint linkStatus = GL_FALSE;
+        glGetProgramiv(program, GL_LINK_STATUS, &linkStatus);
+        if (linkStatus != GL_TRUE) {
+            GLint bufLength = 0;
+            glGetProgramiv(program, GL_INFO_LOG_LENGTH, &bufLength);
+            if (bufLength) {
+                char *buf = (char *) malloc(bufLength);
+                if (buf) {
+                    glGetProgramInfoLog(program, bufLength, NULL, buf);
+                    LOG("Could not link program:\n%s\n", buf);
+                    free(buf);
+                }
+            }
+            glDeleteProgram(program);
+            program = 0;
+        }
+    }
+    return program;
+}
 
 void init(JNIEnv *env, jobject javeSurface) {
     EGLDisplay eglDisplay;
@@ -42,7 +128,6 @@ void init(JNIEnv *env, jobject javeSurface) {
             EGL_CONTEXT_CLIENT_VERSION, 2,
             EGL_NONE
     };
-    EGLContext eglContext;
     if (!(eglContext = eglCreateContext(eglDisplay, eglConfig, nullptr, contextAttribs))) {
         LOG("eglCreateContext get error:%d", eglGetError());
         return;
@@ -51,7 +136,7 @@ void init(JNIEnv *env, jobject javeSurface) {
     EGLint format;
     eglGetConfigAttrib(eglDisplay, eglConfig, EGL_NATIVE_VISUAL_ID, &format);
     ANativeWindow_setBuffersGeometry(nativeWindow, 0, 0, format);
-    EGLSurface eglSurface = eglCreateWindowSurface(eglDisplay, eglConfig, nativeWindow, nullptr);
+    eglSurface = eglCreateWindowSurface(eglDisplay, eglConfig, nativeWindow, nullptr);
     if (!eglSurface) {
         LOG("eglCreateWindowSurface get error: %d", eglGetError());
         return;
@@ -61,13 +146,41 @@ void init(JNIEnv *env, jobject javeSurface) {
         return;
     }
     LOG("native egl init complete");
+    gProgram = createProgram(gVertexShader, gFragmentShader);
+    if (!gProgram) {
+        LOG("Could not create program.");
+        return;
+    }
+    gvPositionHandle = glGetAttribLocation(gProgram, "vPosition");
+    LOG("glGetAttribLocation(\"vPosition\") = %d\n",
+        gvPositionHandle);
+    int viewPortWidth = ANativeWindow_getWidth(nativeWindow);
+    int viewPortHeight = ANativeWindow_getHeight(nativeWindow);
+    glViewport(0, 0, viewPortWidth, viewPortHeight);
+    LOG("OpenGL init complete");
+    while (true) {
+        static float grey;
+        grey += 0.01f;
+        if (grey > 1.0f) {
+            grey = 0.0f;
+        }
+        glClearColor(grey, grey, grey, 1.0f);
+        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(gProgram);
+
+        glVertexAttribPointer(gvPositionHandle, 2, GL_FLOAT, GL_FALSE, 0, gTriangleVertices);
+        glEnableVertexAttribArray(gvPositionHandle);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        eglSwapBuffers(eglDisplay,eglSurface);
+    }
 }
 
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_example_lfplayer_NativeOpenGLActivity_nativeSurfaceCreated(JNIEnv *env, jobject thiz,
-                                                              jobject surface) {
+                                                                    jobject surface) {
     init(env, surface);
 }
 
